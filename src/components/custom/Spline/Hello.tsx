45f6f0329c850a304993abc60c5eb0119d029c87
virajbhartiya <vlbhartiya@gmail.com>
2024-05-09 13:34:53 +0530

    remove hello spline

--- src/components/custom/Spline/Hello.tsx
+++
@@ -1,8 +0,0 @@
-import Spline from '@splinetool/react-spline'
-
-
- export const Hello = () => {
-  return (
-    <Spline scene="https://prod.spline.design/g1LAgJtyG8terRQe/scene.splinecode" />
-  );
- }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,12 @@
 import { About } from '@/components/Home/About'
 import Layout from '@/components/Layout'
 import { LanguageMaqruee } from '@/components/custom/LanguageMarquee'
-import { Hello } from '@/components/custom/Spline/Hello'
 
 export const Home = () => {
   return (
     <Layout>
       <About/>
       <LanguageMaqruee />
-      <Hello />
     </Layout>
   )
 }
