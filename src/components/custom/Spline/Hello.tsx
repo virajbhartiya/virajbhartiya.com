2b4f63365f3d93c2ec52b1a8b1c2fb6207c0c72b
virajbhartiya <vlbhartiya@gmail.com>
2024-05-09 18:04:10 +0530

    add hello

---
+++ src/components/custom/Spline/Hello.tsx
@@ -0,0 +1,8 @@
+import Spline from '@splinetool/react-spline'
+
+
+ export const Hello = () => {
+  return (
+    <Spline scene="https://prod.spline.design/5FXqsFsRkQCB37UF/scene.splinecode" />
+  );
+ }
