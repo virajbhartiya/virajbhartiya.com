e9f6e730d33d7b0a3a326d112545450025029d10
virajbhartiya <vlbhartiya@gmail.com>
2024-05-10 00:51:22 +0530

    fix footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -4,7 +4,7 @@ import { HeartIcon } from 'lucide-react'
 
 export function Footer() {
   return (
-    <footer className="flex flex-col gap-2 sm:flex-row w-full shrink-0 items-center justify-center py-2 px-4 md:px-6">
+    <footer className="flex gap-2 sm:flex-row w-full shrink-0 items-center justify-center py-2 px-4 md:px-6">
         Made with <HeartIcon size={16} color='#00efa6' /> by <Link to={'/'}><UnderlineHover text='Viraj Bhartiya' /></Link>
     </footer>
   )
