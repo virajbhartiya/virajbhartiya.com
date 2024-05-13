6d928aa009ad80b734dc52e71ae02758f47b2252
virajbhartiya <vlbhartiya@gmail.com>
2024-05-14 01:35:16 +0530

    add scrambler and phone to footer

--- src/components/Marquees/ContactMe.tsx
+++ src/components/Marquees/ContactMe.tsx
@@ -1,25 +1,32 @@
 import Marquee from "react-fast-marquee";
+import Scrambler from "../custom/Scrambler";
 
 export function ContactMarquee() {
   const elements = [
-    <p className="accent proto text-2xl md:text-6xl m-6">
-      Let's Create Something TOgether
-    </p>,
-    <p className="proto bg-[var(--accent)] text-black text-2xl md:text-6xl">
-      Email me
-    </p>,
+    <p className="accent proto text-2xl md:text-6xl m-6">Freelance?</p>,
+    <a href="mailto:vlbhartiya@gmail.com">
+      <p className="proto bg-[var(--accent)] text-black text-2xl md:text-6xl">
+        <Scrambler duration={5} text="Email me" />
+      </p>
+    </a>,
+    <p className="accent proto text-2xl md:text-6xl m-6">Collaboration?</p>,
+    <a href="tel:+918485837871">
+      <p className="proto bg-[var(--accent)] text-black text-2xl md:text-6xl">
+        <Scrambler duration={5} text="Call Me"></Scrambler>
+      </p>
+    </a>,
   ];
 
   return (
     <a>
       <div className="w-full h-0.5 bg-[var(--accent)]" />
-      <a href="mailto:vlbhartiya@gmail.com">
-        <div className="relative overflow-hidden ">
-          <Marquee pauseOnHover={true} speed={150}>
-            {elements.map((element) => element)}
-          </Marquee>
-        </div>
-      </a>
+
+      <div className="relative overflow-hidden ">
+        <Marquee pauseOnHover={true} speed={150}>
+          {elements.map((element) => element)}
+        </Marquee>
+      </div>
+
       <div className="w-full h-0.5 bg-[var(--accent)]" />
     </a>
   );
--- src/components/custom/Scrambler.tsx
+++ src/components/custom/Scrambler.tsx
@@ -1,13 +1,13 @@
 import { useScramble } from "use-scramble";
 
-const Scrambler = ({ text }: { text: string }) => {
+const Scrambler = ({ text, duration }: { text: string; duration?: number }) => {
   const { ref, replay } = useScramble({
     text: text,
     range: [65, 125],
-    speed: 0.2,
+    speed: 0.4,
     tick: 1,
     step: 5,
-    scramble: Infinity,
+    scramble: duration ? duration : Infinity,
     seed: 2,
     chance: 1,
     overdrive: true,
