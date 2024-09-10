c7db180fe4246cc01597b283e9f99b532d148faf
Viraj Bhartiya <vlbhartiya@gmail.com>
2024-09-11 00:03:17 +0530

    bye bye cursor

--- src/Hook/useLenis.tsx
+++ src/Hook/useLenis.tsx
@@ -10,10 +10,6 @@ export default function UseLenis() {
       easing: (x: number) => 1 - Math.pow(1 - x, 5),
     });
 
-    lenis.on("scroll", (e: any) => {
-      console.log(e);
-    });
-
     function raf(time: number) {
       lenis.raf(time);
       requestAnimationFrame(raf);
