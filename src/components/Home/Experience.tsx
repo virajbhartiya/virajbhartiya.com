2e69ba9717e220246e8ac77b65cf47b9278fc5da
Viraj Bhartiya <vlbhartiya@gmail.com>
2024-11-22 22:06:16 +0530

    Update Experience.tsx

--- src/components/Home/Experience.tsx
+++ src/components/Home/Experience.tsx
@@ -5,7 +5,7 @@ const experiences = [
     description: [
       "Contributed to the development of Lotus, the Go-based reference implementation of the Filecoin Protocol, a decentralized storage network.",
       "Collaborated with ChainSafe to enhance Forest, the Rust-based implementation of the Filecoin Protocol.",
-      "Developed Ethereum RPC integrations to simplify interactions between Ethereum developers and the Filecoin network.",
+      "Developed Ethereum RPC methods to simplify interactions between Ethereum developers and the Filecoin network.",
     ],
     badge: "Oct 2024 - Present",
   },
